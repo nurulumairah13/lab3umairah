b8a9cea15a6fc57a47af95169c2ac40451d77e7f
HPUser <2022616642@student.uitm.edu.my>
2023-11-27 16:32:20 +0800

    Commit SQLite (add button, filling form, save information, update data, display information,delete information)

---
+++ app/src/main/java/com/example/lab3umairah2nd/CreateBiodataActivity.java
@@ -0,0 +1,14 @@
+package com.example.lab3umairah2nd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateBiodataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_biodata);
+    }
+}
---
+++ app/src/main/java/com/example/lab3umairah2nd/ViewBiodataActivity.java
@@ -0,0 +1,14 @@
+package com.example.lab3umairah2nd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewBiodataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_biodata);
+    }
+}
